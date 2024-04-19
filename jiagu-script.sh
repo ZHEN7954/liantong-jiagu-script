76f42a6d50a545598afa6e36c7989f906dba89b5
ZHEN7954 <79548327@QQ.COM>
2024-04-19 14:07:16 +0800

    Update jiagu-script.sh

--- jiagu-script.sh
+++ jiagu-script.sh
@@ -54,7 +54,7 @@ check_null_passwd_uid_0(){
 
 
 centos7_add_password_policy(){
-  echo ">>>>>>>>>>>>>>>>><<<<<>centos7添加密码策略<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+  echo ">>>>>>>>>>>>>>>>>>>>>>centos7添加密码策略<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
   cp /etc/security/pwquality.conf{,.bak}
 #  cat /etc/security/pwquality.conf | grep -e "remember\|deny\|difok\|minlen\|lcredit\|ucredit\|dcredit\|ocredit\|unlock_time" | grep -v '#'
   sed -i 's/^\([^#]\)/# \1/g' /etc/security/pwquality.conf
@@ -73,7 +73,7 @@ EOF
 }
 
 check_wheel(){
-  echo ">>>>>>>>>>>>>>>>><<<<<>>>>>检查wheel有无app_999<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+  echo ">>>>>>>>>>>>>>>>>>>>>>>>检查wheel有无app_999<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
   getent group wheel | grep 'app_999'  > /dev/null
   if [ $? -ne 0 ];then
     usermod -G wheel app_999
@@ -95,16 +95,139 @@ check_wheel(){
 
 
 
+check_vsftp_service(){
+  echo ">>>>>>>>>>>>>>>>>>>>>>>检查vsftp服务<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+  # 检查是否安装了 vsftpd
+  if ! command -v vsftpd &> /dev/null; then
+      echo "vsftpd 未安装"
+  else
+      echo "vsftpd 已安装，请配置限制条件"
+      if grep -q "^root$" /etc/vsftpd/ftpusers; then
+      	echo 'root alread no'
+      else
+      	echo "root" >> /etc/vsftpd/ftpusers 
+      	echo 'root alread add'
+      fi
+      if grep -q "^anon_umask=022$" /etc/vsftpd/vsftpd.conf && grep -q "^local_umask=022$" /etc/vsftpd/vsftpd.conf; then
+      echo "已经存在 anon_umask=022 和 local_umask=022 两行"
+      grep -E '^(anon|local)_umask=022$' /etc/vsftpd/vsftpd.conf | grep -v '#'
+  		else
+  		    { grep -q "^anon_umask=022$" /etc/vsftpd/vsftpd.conf || echo "anon_umask=022"; grep -q "^local_umask=022$" /etc/vsftpd/vsftpd.conf || echo "local_umask=022"; } | sudo tee -a /etc/vsftpd/vsftpd.conf >/dev/null
+  		    echo "已添加缺失的行到 /etc/vsftpd/vsftpd.conf"
+  		    grep -E '^(anon|local)_umask=022$' /etc/vsftpd/vsftpd.conf | grep -v '#'
+  				# 重启 vsftpd 服务
+  				systemctl restart vsftpd
+  				if [ $? -eq 0 ]; then
+  					    echo "vsftpd 服务已成功重启"
+  					else
+  					    echo "重启 vsftpd 服务时出现错误请检查"
+  				fi
+  		fi
+  fi
+}
+
+
+add_sysfile_authority(){
+  echo ">>>>>>>>>>>>>>>>>>>>>>>>>检查系统文件权限<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+  chmod 644 /etc/passwd
+  chmod 400 /etc/shadow
+  chmod 644 /etc/group
+  chmod 600 /var/log/messages
+  chmod 644 /var/log/dmesg
+  chmod 600 /var/log/maillog
+  chmod 600 /var/log/secure 
+  chmod 664 /var/log/wtmp
+  chmod 600 /var/log/cron
+
+  ls -l /etc/passwd
+  ls -l /etc/shadow
+  ls -l /etc/group
+  ls -l /var/log/messages
+  ls -l /var/log/dmesg
+  ls -l /var/log/maillog
+  ls -l /var/log/secure 
+  ls -l /var/log/wtmp
+  ls -l /var/log/cron
+}
+
+ssh_config_reinforce(){
+  echo ">>>>>>>>>>>>>>>>>>>>>>>ssh 加固<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+  cp /etc/ssh/sshd_config{,.bak}
+  # 取消 LogLevel INFO 参数的注释
+  sed  -i '/^#LogLevel INFO/s/^#//' /etc/ssh/sshd_config
+  cat /etc/ssh/sshd_config | grep evel
+
+  #原有的全注释
+  sed -i '/^ClientAliveInterval /s/^/#/' /etc/ssh/sshd_config
+  sed -i '/^ClientAliveCountMax /s/^/#/' /etc/ssh/sshd_config
+  sed -i '/^MaxAuthTries /s/^/#/' /etc/ssh/sshd_config
+  sed -i '/^IgnoreRhosts /s/^/#/' /etc/ssh/sshd_config
+  sed -i '/^HostbasedAuthentication /s/^/#/' /etc/ssh/sshd_config
+  sed -i '/^PermitEmptyPasswords /s/^/#/' /etc/ssh/sshd_config
+  sed -i '/^PermitRootLogin /s/^/#/' /etc/ssh/sshd_config
+  sed -i '/^X11Forwarding /s/^/#/' /etc/ssh/sshd_config
+  #全部一次性追加
+  cat >> /etc/ssh/sshd_config << EOF
+ClientAliveInterval 600
+ClientAliveCountMax 2
+MaxAuthTries 4
+IgnoreRhosts yes
+HostbasedAuthentication no
+PermitEmptyPasswords no
+PermitRootLogin no
+X11Forwarding no 
+EOF
+  tail -n 10 /etc/ssh/sshd_config
+  systemctl restart sshd
+  systemctl status  sshd
+}
+
+
+check_telnet_service(){
+  echo ">>>>>>>>>>>>>>>>>>>>>>>检查是否开启了Telnet-Server服务<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+  if ss -tnlp  | grep 23; then
+    echo ">>>Telnet-Server服务已开启"
+  else
+    echo "Telnet-Server服务未开启--------[无需调整]"
+  fi
+}
+
+config_log(){
+  echo ">>>>>>>>>>>>>>>>>>>>>>>配置日志服务<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+  #=========rsylog
+  systemctl enable rsyslog
+  echo "------------检查rsyslog日志配置-------------------"
+  cat /etc/rsyslog.conf | grep -e "secure\|cron"  | grep -v '#'
+  
+  
+  #======lograte
+  echo ">>>>>>>>>>>>>>>>>>>>>>>配置lograte日志切割<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+  cp /etc/logrotate.conf{,.bak}
+  cat /etc/logrotate.conf
+  sed -i 's/rotate [0-9]/# rotate 1/' /etc/logrotate.conf
+  sed -i '1 a\rotate 26' /etc/logrotate.conf
+  cat /etc/logrotate.conf
+  systemctl restart rsyslog.service
+}
+
+add_kernel_parameter(){
+  echo ">>>>>>>>>>>>>>>>>>>>>>>内核参数配置<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+  if ! grep -q "kernel.randomize_va_space" /etc/sysctl.conf; then
+      echo "kernel.randomize_va_space = 2" | sudo tee -a /etc/sysctl.conf
+  fi
+  sysctl -w kernel.randomize_va_space=2
+}
 
 #############应用函数###################
 check_null_passwd_uid_0
 # centos7 可用
 centos7_add_password_policy
 check_wheel
-
-
-
-
+check_vsftp_service
+add_sysfile_authority
+ssh_config_reinforce
+check_telnet_service
+add_kernel_parameter
 
 
 
