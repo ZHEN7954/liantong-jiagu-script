7d529d508d3d273e4ee6a94557600b6be256f521
ZHEN7954 <79548327@QQ.COM>
2024-04-22 20:46:48 +0800

    Update jiagu-script.sh

--- jiagu-script.sh
+++ jiagu-script.sh
@@ -10,7 +10,7 @@ echo " "
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>系统基本信息<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
 hostname=$(uname -n)
 system=$(cat /etc/os-release | grep "^NAME" | awk -F\" '{print $2}')
-version=$(cat /etc/redhat-release | awk '{print $4$5}')
+version=$(cat /etc/redhat-release|sed -r 's/.* ([0-9]+)\..*/\1/') #rpm -q centos-release|cut -d- -f3
 kernel=$(uname -r)
 platform=$(uname -p)
 address=$(ip addr | grep inet | grep -v "inet6" | grep -v "127.0.0.1" | awk '{ print $2; }' | tr '\n' '\t' )
@@ -30,18 +30,20 @@ echo "CPU核数:          $cpu"
 echo "机器型号:         $machinemodel"
 echo "系统时间:         $date"
 echo " "
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>资源使用情况<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-summemory=$(free -h |grep "Mem:" | awk '{print $2}')
-freememory=$(free -h |grep "Mem:" | awk '{print $4}')
-usagememory=$(free -h |grep "Mem:" | awk '{print $3}')
-uptime=$(uptime | awk '{print $2" "$3" "$4" "$5}' | sed 's/,$//g')
-loadavg=$(uptime | awk '{print $9" "$10" "$11" "$12" "$13}')
+#echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>资源使用情况<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+#summemory=$(free -h |grep "Mem:" | awk '{print $2}')
+#freememory=$(free -h |grep "Mem:" | awk '{print $4}')
+#usagememory=$(free -h |grep "Mem:" | awk '{print $3}')
+#uptime=$(uptime | awk '{print $2" "$3" "$4" "$5}' | sed 's/,$//g')
+#loadavg=$(uptime | awk '{print $9" "$10" "$11" "$12" "$13}')
 
-echo "总内存大小:           $summemory"
-echo "已使用内存大小:       $usagememory"
-echo "可使用内存大小:       $freememory"
-echo "系统运行时间:         $uptime"
-echo "系统负载:             $loadavg"
+#echo "总内存大小:           $summemory"
+#echo "已使用内存大小:       $usagememory"
+#echo "可使用内存大小:       $freememory"
+#echo "系统运行时间:         $uptime"
+#echo "系统负载:             $loadavg"
+#echo " "
+
 
 
 check_null_passwd_uid_0(){
@@ -50,13 +52,27 @@ check_null_passwd_uid_0(){
   echo "空密码用户:    $nullpasswd"
   uid0user=$(awk -F: '($3 == 0) {print}' /etc/passwd)
   echo "uid=0用户：    $uid0user"
+  echo " "
+}
+
+centos6_add_password_policy(){
+  echo ">>>>>>>>>>>>>>>>>>>>>>centos6添加密码策略<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+  cp /etc/pam.d/password-auth{,.bak}
+  cp /etc/pam.d/system-auth{,.bak}
+  sed -i 's/password    requisite     pam_cracklib.so try_first_pass retry=3/password    requisite     pam_cracklib.so minlen=8 dcredit=-1 ucredit=-1 ocredit=-1 lcredit=-1 minclass=4 retry=3/g' /etc/pam.d/system-auth
+  #不重复使用最近使用过的密码
+
+auth        required      pam_unix.so try_first_pass likeauth nullok remember=5 lockout_time=900
+
+  
+  sed -i '/^password.*sufficient.*pam_unix.so/s/$/ remember=5/' /etc/pam.d/password-auth
+  sed -i '/^password.*sufficient.*pam_unix.so/s/$/ remember=5/' /etc/pam.d/system-auth
 }
 
 
 centos7_add_password_policy(){
   echo ">>>>>>>>>>>>>>>>>>>>>>centos7添加密码策略<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
   cp /etc/security/pwquality.conf{,.bak}
-#  cat /etc/security/pwquality.conf | grep -e "remember\|deny\|difok\|minlen\|lcredit\|ucredit\|dcredit\|ocredit\|unlock_time" | grep -v '#'
   sed -i 's/^\([^#]\)/# \1/g' /etc/security/pwquality.conf
   cat >> /etc/security/pwquality.conf << EOF
 remember = 5
@@ -70,27 +86,36 @@ ocredit = -1
 unlock_time = 900
 EOF
   cat  /etc/security/pwquality.conf | grep -v '#'
+  echo " "
 }
 
 check_wheel(){
   echo ">>>>>>>>>>>>>>>>>>>>>>>>检查wheel有无app_999<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
   getent group wheel | grep 'app_999'  > /dev/null
-  if [ $? -ne 0 ];then
+  if [ $? -eq 0 ];then
+    echo "app_999 already to wheel group"
+    getent group wheel | grep 'app_999'
+    echo " "
+  else
     usermod -G wheel app_999
     echo "app_999 add to wheel group"
-  else
     getent group wheel | grep 'app_999'
+    echo " "
   fi
-    echo ">>>>>>>>>>>>>>>>><检查/etc/pam.d/su 已添加wheel<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+  
+  echo ">>>>>>>>>>>>>>>>><检查/etc/pam.d/su 已添加wheel<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
   cp /etc/pam.d/su{,.bak}
   if grep -q "group=wheel" /etc/pam.d/su; then
-      echo 'youle '
+      echo 'Already Existing'
+      echo " "
   else
       sed -i 's/#auth\s\+required\s\+pam_wheel\.so\s\+use_uid/auth\t\trequired\tpam_wheel.so group=wheel/'  /etc/pam.d/su
-      echo 'yi tianjia'
+      echo 'Added to /etc/pam.d/su'
+      
   fi
 
   cat /etc/pam.d/su
+  echo " "
 }
 
 
@@ -100,27 +125,29 @@ check_vsftp_service(){
   # 检查是否安装了 vsftpd
   if ! command -v vsftpd &> /dev/null; then
       echo "vsftpd 未安装"
+      echo " "
   else
       echo "vsftpd 已安装，请配置限制条件"
       if grep -q "^root$" /etc/vsftpd/ftpusers; then
-      	echo 'root alread no'
+      	echo 'root user disabled'
       else
       	echo "root" >> /etc/vsftpd/ftpusers 
-      	echo 'root alread add'
+      	echo 'root  add to ftpusers'
       fi
       if grep -q "^anon_umask=022$" /etc/vsftpd/vsftpd.conf && grep -q "^local_umask=022$" /etc/vsftpd/vsftpd.conf; then
-      echo "已经存在 anon_umask=022 和 local_umask=022 两行"
+      echo "Already exists anon_umask=022 和 local_umask=022 2row"
       grep -E '^(anon|local)_umask=022$' /etc/vsftpd/vsftpd.conf | grep -v '#'
   		else
   		    { grep -q "^anon_umask=022$" /etc/vsftpd/vsftpd.conf || echo "anon_umask=022"; grep -q "^local_umask=022$" /etc/vsftpd/vsftpd.conf || echo "local_umask=022"; } | sudo tee -a /etc/vsftpd/vsftpd.conf >/dev/null
-  		    echo "已添加缺失的行到 /etc/vsftpd/vsftpd.conf"
+  		    echo "Added missing rows to /etc/vsftpd/vsftpd.conf"
   		    grep -E '^(anon|local)_umask=022$' /etc/vsftpd/vsftpd.conf | grep -v '#'
   				# 重启 vsftpd 服务
   				systemctl restart vsftpd
   				if [ $? -eq 0 ]; then
-  					    echo "vsftpd 服务已成功重启"
+  					    echo "vsftpd Service restart successful"
+                echo " "
   					else
-  					    echo "重启 vsftpd 服务时出现错误请检查"
+  					    echo "vsftpd Service restart failed"
   				fi
   		fi
   fi
@@ -139,15 +166,16 @@ add_sysfile_authority(){
   chmod 664 /var/log/wtmp
   chmod 600 /var/log/cron
 
+  ls -l /var/log/wtmp
   ls -l /etc/passwd
-  ls -l /etc/shadow
   ls -l /etc/group
-  ls -l /var/log/messages
   ls -l /var/log/dmesg
+  ls -l /etc/shadow
+  ls -l /var/log/messages
   ls -l /var/log/maillog
   ls -l /var/log/secure 
-  ls -l /var/log/wtmp
   ls -l /var/log/cron
+  echo " "
 }
 
 ssh_config_reinforce(){
@@ -156,6 +184,8 @@ ssh_config_reinforce(){
   # 取消 LogLevel INFO 参数的注释
   sed  -i '/^#LogLevel INFO/s/^#//' /etc/ssh/sshd_config
   cat /etc/ssh/sshd_config | grep evel
+  echo "-------Cancel LogLevel INFO--------[已配置]"
+  echo " "
 
   #原有的全注释
   sed -i '/^ClientAliveInterval /s/^/#/' /etc/ssh/sshd_config
@@ -180,15 +210,22 @@ EOF
   tail -n 10 /etc/ssh/sshd_config
   systemctl restart sshd
   systemctl status  sshd
+  echo " "
 }
 
 
 check_telnet_service(){
   echo ">>>>>>>>>>>>>>>>>>>>>>>检查是否开启了Telnet-Server服务<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
   if ss -tnlp  | grep 23; then
-    echo ">>>Telnet-Server服务已开启"
+    echo ">>>Telnet-Server服务已开启--------[需调整]"
+    sed -i 's/disable = no/disable = yes/' /etc/xinetd.d/telnet
+    systemctl stop telnet
+    systemctl disable telnet
+    service telnet stop
+    chkconfig telnet off
   else
     echo "Telnet-Server服务未开启--------[无需调整]"
+    echo " "
   fi
 }
 
@@ -204,6 +241,7 @@ config_log(){
   echo ">>>>>>>>>>>>>>>>>>>>>>>配置lograte日志切割<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
   cp /etc/logrotate.conf{,.bak}
   cat /etc/logrotate.conf
+ echo " "
   sed -i 's/rotate [0-9]/# rotate 1/' /etc/logrotate.conf
   sed -i '1 a\rotate 26' /etc/logrotate.conf
   cat /etc/logrotate.conf
@@ -219,14 +257,15 @@ add_kernel_parameter(){
 }
 
 #############应用函数###################
-check_null_passwd_uid_0
 # centos7 可用
+check_null_passwd_uid_0
 centos7_add_password_policy
 check_wheel
 check_vsftp_service
 add_sysfile_authority
 ssh_config_reinforce
 check_telnet_service
+config_log
 add_kernel_parameter
 
 
